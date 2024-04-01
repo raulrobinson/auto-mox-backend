ee9c73d3a922f4601370c8df82dfd9c8eafe18c3
rasysbox <rasysbox@hotmail.com>
2024-04-01 10:22:33 -0500

    fc

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ app.get('/', (req, res) => {
 
 require('./app/routes/routes')(app);
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
